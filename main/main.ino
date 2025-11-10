ce25f802d8646e3f45ecbe11e5ca69c39c25e335
Thomas <thomasnagelgaard10@gmail.com>
2025-11-10 17:38:59 +0100

    store changes
    endret blant annet Arduino.STL, lagde stepper styring, sletta gammel junk

--- main/main.ino
+++ main/main.ino
@@ -1,15 +1,33 @@
-#include "queue.h"
-#include "motor.h"
-#include "styring.h"
+#include <Arduino.h>
+#include "stepper.h"
 
+Stepper stepper;
 
-void setup()
-{
+void setup() {
+    // Initialize stepper motor
     Serial.begin(9600);
+    stepper.init();
+    stepper.setCurrent(0.5,0.5);
 }
 
+void loop() {
+    stepper.update();
 
-void loop()
-{
-
+    if (Serial.available()) {
+        char cmd = Serial.read();
+        switch(cmd) {
+            case 'o': // open
+                Serial.println("Opening door");
+                stepper.doorOpen();
+                break;
+            case 'c': // close
+                Serial.println("Closing door");
+                stepper.doorClose();
+                break;
+            case 's': // stop
+                Serial.println("Stopping door");
+                stepper.doorStop();
+                break;
+        }
+    }
 }
---
+++ main/stepper.h
@@ -0,0 +1,50 @@
+#ifndef STEPPER_H
+#define STEPPER_H
+
+#include <arduino.h>
+#include <ArduinoSTL.h> //comment out "const std::nothrow_t std::nothrow = { };" in "libraries\ArduinoSTL\src\new_handler.cpp"
+#include <array>
+#include <LiquidCrystal.h>
+#include <vector>
+#include <TimerOne.h>
+
+class Stepper {
+public:
+    Stepper():
+        aPhase{A14},
+        aEnable{A15},
+        bPhase{A12},
+        bEnable{A13},
+        position{0}
+    {}
+    const uint8_t aPhase;
+    const uint8_t aEnable;
+    const uint8_t bPhase;
+    const uint8_t bEnable;
+    void init();
+    void setCurrent(float aCurrent, float bCurrent);
+    void halfStep(bool dir);
+    void doorOpen();
+    void doorClose();
+    void doorStop();
+    void update();
+    uint32_t position;
+
+    enum State {
+        STOPPED,
+        OPENING,
+        CLOSING
+    };
+    void stop();
+    State getState() { return currentState; }
+
+private:
+    inline void ldac(bool val)  {(val ? (PORTC |= 0B10000000) : (PORTC &= 0B01111111));}
+    inline void sdi(bool val)   {(val ? (PORTC |= 0B01000000) : (PORTC &= 0B10111111));}
+    inline void sck(bool val)   {(val ? (PORTC |= 0B00100000) : (PORTC &= 0B11011111));}
+    inline void cs(bool val)    {(val ? (PORTC |= 0B00010000) : (PORTC &= 0B11101111));}
+    inline void clockPulse() {sck(HIGH); sck(LOW);}
+    volatile State currentState;
+};
+
+#endif
