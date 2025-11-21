6fe75caffa3fe3dab24ca4776f1b5bab3ab323d8
Thomas <thomasnagelgaard10@gmail.com>
2025-11-21 15:25:55 +0100

    Final matlab monitor
    no more monitoring changes needed in matlab, only figures and plotting changes

--- monitor.m
+++ monitor.m
@@ -4,8 +4,8 @@ name = 'COM5';
 rate = 9600;
 s = serialport(name,rate,"Timeout", 2);
 
-nsamples = 5000;
-data = zeros(nsamples,5);
+nsamples = 8000; % how many data points you want available
+data = zeros(nsamples,6);
 timeVals = zeros(nsamples,1);
 flush(s);
 
@@ -29,11 +29,11 @@ for k = 1:nsamples
     if numel(nums) == 1 && ~isnan(nums(1))
         enc = nums(1);
         % fill all 5 columns with encoder-only info
-        data(k, :) = [NaN, NaN, NaN, NaN, enc];
+        data(k, :) = [NaN, NaN, NaN, NaN, NaN, enc];
 
     % Case 2: Arduino outputs full "a,b,c,d,e"
-    elseif numel(nums) >= 5 && all(~isnan(nums(1:5)))
-        data(k, :) = nums(1:5).';
+    elseif numel(nums) >= 6 && all(~isnan(nums(1:6)))
+        data(k, :) = nums(1:6).';
 
     % Case 3: broken line → repeat previous
     elseif k > 1
@@ -46,77 +46,48 @@ end
 clear s;
 %%
 clc; close all; 
-elapTime = data(:,1);
-desPos = data(:,2);
-desVel = data(:,3);
-dest = data(:,4);
-encCount = data(:,5);
-Pos = encCount./8384;
+power = data(:,1); %Pådrag fra PID
+start = 1; 
+endP = 669;
+
+elapTime = data([start:endP],2);
+elapTime = timeVals(start:endP);
+desPos = data([start:endP],3);
+desVel = data([start:endP],4);
+dest = data([start:endP],5);
+encCount = data([start:endP],6);
+Pos = encCount;
+
+Pos(203) = Pos(202);
+Pos(397:398) = Pos(396);
+%Pos(end) = Pos(end-1);
 
 fcut = 1e-50;
 fs = 1/mean(diff(elapTime));
 
-PosFilt = lowpass(Pos,fcut,fs)
+PosFilt = lowpass(Pos,fcut,fs);
+
+vel = gradient(PosFilt,elapTime);
+VelFilt = lowpass(vel,fcut,fs);
+acc = gradient(VelFilt, elapTime);
+accFilt = lowpass(acc,fcut,fs);
 
 figure
-plot(elapTime, desPos,LineWidth=3)
+plot(elapTime, desPos,LineWidth=4)
+hold on
+plot(elapTime,Pos,Color='r', LineWidth=2)
 ylabel('Desired Position')
-ylim([0 7])
+ylim([-0.1 7.2])
 hold on 
 yyaxis("right")
 ylim([-1 1])
 plot(elapTime, desVel,LineWidth=3)
-ylabel('Desired Velocity')
-%hold on
-%plot(elapTime, Pos)
-grid on
-xlabel('Time(s)')
-legend('Desired Position','Desired Velocity')
-
-%%
-clc; close all; 
-CPR = 8384;
-dataCorr = data/CPR; %Position in rotation/floor
-dataAng = dataCorr*(2*pi);
-
-fs = 1/mean(diff(timeVals));
-fcut = 1e-50; 
-%fcut = 50;
-
-dataAngFiltered = lowpass(dataAng,fcut,fs);
-
-%finding the velocity and filtering
-vel = gradient(dataAngFiltered,timeVals);
-velFiltered = lowpass(vel,fcut,fs);
-
-%finding the acceleration and filtering
-acc = gradient(velFiltered,timeVals);
-accFiltered = lowpass(acc,fcut,fs);
-
-
-
-figure
-subplot(2,1,1)
-plot(timeVals,dataCorr,LineWidth=2,Color="#FFC0CB")
-ylabel('Floor')
-xlabel('Time(s)')
-ylim([0 87])
-xlim([0 50])
-yticks(0:10:87)
-grid on
-
-subplot(2,1,2)
-yyaxis("left")
-plot(timeVals,velFiltered)
-ylabel('Velocity(rad/s)')
-ylim([-10 10])
-xlim([0 50])
-
 hold on
-yyaxis("right")
-plot(timeVals,accFiltered)
-ylabel('Acceleration(rad/s^2)')
-ylim([-40 40])
+plot(elapTime, vel,LineStyle="-",Color='g',LineWidth=2)
+hold on
+plot(elapTime, accFilt, LineStyle= "-", Color='m',LineWidth=2 )
+ylabel('Desired Velocity')
 
-xlabel('Time(s)')
 grid on
+xlabel('Time(s)')
+legend('Desired Position','Pos','Desired Velocity','Velocity','Acceleration')
